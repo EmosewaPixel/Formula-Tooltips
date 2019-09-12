890d2f6d32493a9eaff2771f28579c9cadc7c734
EmosewaPixel <awesomeggaming@gmail.com>
2019-09-12 21:53:12 +0200

    A slight library update

--- src/main/java/com/novumscientiateam/formulatooltips/FormulaTooltips.kt
+++ src/main/java/com/novumscientiateam/formulatooltips/FormulaTooltips.kt
@@ -30,42 +30,41 @@ object GameEvents {
         val item = e.itemStack.item
         if (item is BlockItem) {
             val block = Block.getBlockFromItem(item)
-            if (MaterialBlocks.getAll().contains(block)) {
+            if (block in MaterialBlocks) {
                 formula = getFormulaString(MaterialBlocks.getBlockMaterial(block)!!)
                 if (formula != "?")
                     e.toolTip.add(StringTextComponent(TextFormatting.GRAY.toString() + formula))
             }
-        } else if (MaterialItems.getAll().contains(item)) {
+        } else if (item in MaterialItems) {
             formula = getFormulaString(MaterialItems.getItemMaterial(item)!!)
             if (formula != "?")
                 e.toolTip.add(StringTextComponent(TextFormatting.GRAY.toString() + formula))
         }
     }
 
-    private fun getFormulaString(mat: Material): String {
-        return if (mat.composition.size == 0)
-            mat.element.symbol
-        else
-            mat.composition.map { (material, count) ->
-                if (material === MaterialRegistry.WATER)
-                    "·" + count + "H2O"
-                if (material is GroupMaterial)
-                    if (count > 1)
-                        "(" + ElementUtils.getElementalComposition(material).map { s -> s.element.symbol + if (s.count > 1) s.count else "" }.joinToString { it } + ")" + count
-                    else
-                        ElementUtils.getElementalComposition(material).map { s -> s.element.symbol + s.count }.joinToString { it }
-                val composition = material.composition
-                if (composition.size == 1) {
-                    if (composition[0].material is TransitionMaterial)
-                        getFormulaString(composition[0].material) + count * composition[0].count
-                    getFormulaString(material) + count
-                }
-                if (composition.size > 1)
-                    if (count > 1)
-                        "(" + getFormulaString(material) + ")" + count
-                    else
-                        getFormulaString(material)
-                material.element.symbol + if (count > 1) count else ""
-            }.joinToString { it }
-    }
+    private fun getFormulaString(mat: Material): String =
+            if (mat.composition.isEmpty())
+                mat.element.symbol
+            else
+                mat.composition.map { (material, count) ->
+                    if (material === MaterialRegistry.WATER)
+                        "·" + count + "H2O"
+                    if (material is GroupMaterial)
+                        if (count > 1)
+                            "(${ElementUtils.getElementalComposition(material).map { it.element.symbol + if (it.count > 1) it.count else "" }.joinToString(separator = "", transform = { it })})$count"
+                        else
+                            ElementUtils.getElementalComposition(material).map { it.element.symbol + it.count }.joinToString(separator = "", transform = { it })
+                    val composition = material.composition
+                    if (composition.size == 1) {
+                        if (composition[0].material is TransitionMaterial)
+                            getFormulaString(composition[0].material) + count * composition[0].count
+                        getFormulaString(material) + count
+                    }
+                    if (composition.isNotEmpty())
+                        if (count > 1)
+                            "(${getFormulaString(material)})$count"
+                        else
+                            getFormulaString(material)
+                    material.element.symbol + if (count > 1) count else ""
+                }.joinToString(separator = "", transform = { it })
 }
