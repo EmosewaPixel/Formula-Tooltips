086a2f17195471f92d36f095280b2bc7e94d19b5
EmosewaPixel <awesomeggaming@gmail.com>
2019-10-03 22:58:12 +0200

    Added percentages for alloys

--- src/main/java/com/novumscientiateam/formulatooltips/FormulaTooltips.kt
+++ src/main/java/com/novumscientiateam/formulatooltips/FormulaTooltips.kt
@@ -1,12 +1,16 @@
 package com.novumscientiateam.formulatooltips
 
+import com.emosewapixel.pixellib.extensions.round
+import com.emosewapixel.pixellib.extensions.shorten
 import com.emosewapixel.pixellib.extensions.toSubscipt
 import com.emosewapixel.pixellib.materialsystem.MaterialRegistry
 import com.emosewapixel.pixellib.materialsystem.element.ElementUtils
 import com.emosewapixel.pixellib.materialsystem.lists.MaterialBlocks
 import com.emosewapixel.pixellib.materialsystem.lists.MaterialItems
+import com.emosewapixel.pixellib.materialsystem.materials.CompoundType
 import com.emosewapixel.pixellib.materialsystem.materials.Material
 import com.emosewapixel.pixellib.materialsystem.materials.utility.GroupMaterial
+import com.emosewapixel.pixellib.materialsystem.materials.utility.MaterialStack
 import com.emosewapixel.pixellib.materialsystem.materials.utility.TransitionMaterial
 import net.alexwells.kottle.KotlinEventBusSubscriber
 import net.minecraft.block.Block
@@ -32,16 +36,35 @@ object GameEvents {
         if (item is BlockItem) {
             val block = Block.getBlockFromItem(item)
             if (block in MaterialBlocks) {
-                formula = getFormulaString(MaterialBlocks.getBlockMaterial(block)!!)
+                val mat = MaterialBlocks.getBlockMaterial(block)!!
+                formula = if (mat.compoundType == CompoundType.ALLOY) getCompositionString(mat) else getFormulaString(mat)
                 if (formula != "?")
                     e.toolTip.add(StringTextComponent(TextFormatting.GRAY.toString() + formula))
             }
         } else if (item in MaterialItems) {
-            formula = getFormulaString(MaterialItems.getItemMaterial(item)!!)
+            val mat = MaterialItems.getItemMaterial(item)!!
+            formula = if (mat.compoundType == CompoundType.ALLOY) getCompositionString(mat) else getFormulaString(mat)
             if (formula != "?")
                 e.toolTip.add(StringTextComponent(TextFormatting.GRAY.toString() + formula))
         }
     }
+
+    private fun getCompositionString(mat: Material, base: Double = 1.0): String =
+            if (mat.composition.isEmpty())
+                mat.element.symbol
+            else {
+                val total = mat.composition.map(MaterialStack::count).sum()
+                mat.composition.map { (material, count) ->
+                    val percent = base * count.toFloat() / total
+                    if (material.composition.isNotEmpty())
+                        if (material.compoundType == CompoundType.ALLOY)
+                            getCompositionString(material, percent)
+                        else
+                            "${(percent * 100).round().shorten()}% ${getFormulaString(material)}"
+                    else
+                        "${(percent * 100).round().shorten()}% ${material.element.symbol}"
+                }.joinToString { it }
+            }
 
     private fun getFormulaString(mat: Material): String =
             if (mat.composition.isEmpty())
@@ -56,7 +79,7 @@ object GameEvents {
                             if (count > 1)
                                 "(${ElementUtils.getElementalComposition(material).map { it.element.symbol + if (it.count > 1) it.count.toSubscipt() else "" }.joinToString(separator = "", transform = { it })})${count.toSubscipt()}"
                             else
-                                ElementUtils.getElementalComposition(material).map { it.element.symbol + it.count.toSubscipt() }.joinToString(separator = "", transform = { it })
+                                ElementUtils.getElementalComposition(material).map { it.element.symbol + it.count.toSubscipt() }.joinToString("") { it }
                         composition.size == 1 ->
                             if (composition[0].material is TransitionMaterial)
                                 getFormulaString(composition[0].material) + (count * composition[0].count).toSubscipt()
@@ -69,5 +92,5 @@ object GameEvents {
                                 getFormulaString(material)
                         else -> material.element.symbol + if (count > 1) count.toSubscipt() else ""
                     }
-                }.joinToString(separator = "", transform = { it })
+                }.joinToString("") { it }
 }
